77167587d18529b20dcafb735207f074f2d50ce4
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-09 00:42:57 +0100

    flaskapp

---
+++ flaskapp.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Saif',
+        'title': 'Vixen',
+        'content': 'Stacy Cruz',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Bala',
+        'title': 'Blacked',
+        'content': 'Tori Black',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    git remote add origin <link>
