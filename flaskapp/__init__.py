0180e7eeae20d7ed240645caa7d9e1b529eb4c5a
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-09 08:40:33 +0100

    Profile pic on account.html

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -11,6 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db=SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
+login_manager.login_message_category = 'info'
+
 
 #Circular import
 from flaskapp import routes
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 #Avoding circular import error
 from flaskapp import app, db, bcrypt
 from flaskapp.forms import RegistrationForm, LoginForm
 from flaskapp.db_models import User,Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 posts = [
     {
@@ -30,6 +31,8 @@ def about():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
        # db.create_all()
@@ -43,13 +46,30 @@ def register():
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user,remember=form.remember.data)
-            return redirect(url_for('home'))
+            #USER LOGGED IN 
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('login unsuccessful :) ','danger')
 
     return render_template('login.html', title = 'Login', form = form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title = "Account")
+
+
