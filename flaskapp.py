c72628985b569a652ba8315e7af886189b18ce73
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-09 01:59:35 +0100

    update forms and add route in flasapp

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '839c91f955762c458c526e67f90a0a68'
 
 posts = [
     {
@@ -16,17 +19,27 @@ posts = [
     }
 ]
 
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/signup", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/signin")
+def signin():
+    form = LoginForm() 
+    return render_template('login.html', title = 'Login', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
