c15c39d4368554fe26f13be5287f132739d3373d
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-19 15:53:34 +0100

    test cases

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -12,17 +12,17 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',[DataRequired(), Length(min = 6), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError("That user name is already taken!")
 
-    def validate_email(self,email):
-        if email.data != current_user.email:
-            email = User.query.filter_by(email=email.data).first()
-            if email:
-                raise ValidationError("That email is already taken!")
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken!')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',[DataRequired(), Email() ])
--- test.py
+++ test.py
@@ -7,34 +7,49 @@ class FlaskTest(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get('/home', content_type='html')
-        self.assertEqual(response.status_code, 200)
-    
+        self.assertFalse(b'New Post' in response.data)
+        
     #about
     def test_about(self):
         tester = app.test_client(self)
-        response = tester.get('/about', content_type='html')
-        self.assertEqual(response.status_code, 200)
-    
-    #signup
-    def test_signup(self):
-        tester = app.test_client(self)
-        response = tester.get('/signup', content_type='html')
-        self.assertEqual(response.status_code, 200)
+        response = tester.get('/about', content_type='html')        
+        self.assertTrue(b'This is about page.' in response.data)
 
-    #signin
-    def test_signin(self):
+    #signin_FAIL
+    def test_login(self):
         tester = app.test_client(self)
-        response = tester.get('/signin', content_type='html')
-        self.assertEqual(response.status_code, 200)
+        tester.post('/signin', data = dict(email = 'kingkocng@gmail.com', password = 'kingkocng@gmail.com'),follow_redirects =True)
+        response = tester.get( '/signin', follow_redirects = True)
+        self.assertIn(b'login unsuccessful :)' , response.data)
 
-    #logout
-    def test_logout(self):
+"""     #signin_SUCCESS
+    def test_login(self):
         tester = app.test_client(self)
-        response = tester.get('/logout', content_type='html')
-        self.assertEqual(response.status_code, 200)
+        response = tester.post('/signin', data = dict(email = 'kingkong@gmail.com', password = 'kingkong@gmail.com'), follow_redirects =True) """
+       # self.assertIn(b'kingkongpost', response.data)
     
+    #register_FAILED
+"""     def test_register(self):
+        tester = app.test_client(self)
+        response = tester.post('/signup', data = dict(username='admin5', email = 'adminsasas@gmail.com', password = 'admin0075', confirm_password = 'admin0075'), follow_redirects =True)
+        response = tester.get(/signup)
+        #self.assertTrue(b'That username is already taken!' in response.data)
+        
+        #self.assertIn(b'Log In', response.data) """
     
+"""     #register_SUCCESS
+    def test_register(self):
+        tester = app.test_client(self)
+        response = tester.post('/signup', data = dict(username='admin6',email = 'admin6@gmail.com', password = 'admin00756', confirm_password = 'admin00756'), follow_redirects =True)
+        self.assertTrue(b'That username is already taken!' in response.data)
 
+        
+        #self.assertTrue(b'Your account has been created, you can now login!' in response.data) 
+ """
+
+
+"""     #Failed_signin  
+ """
 
 if __name__ == '__main__':
     unittest.main()
