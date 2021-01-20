74b72c7f1d49cc80104c261c4a8a904d4d606824
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-20 03:47:17 +0100

    test cases

--- test.py
+++ test.py
@@ -1,61 +1,68 @@
+from flask import Flask, current_app
 from flaskapp.db_models import User
-from flask import current_app
 import unittest
 from flask import request
+import requests
+from flaskapp import db
+
+app = Flask(__name__)
+with app.app_context(): db.create_all()
+
+
 
 def db():
     users_in_db=[]
+
     users = User.query.all()
     for x in users:
         users_in_db.append(x.username)
     return users_in_db
 
+
 class FlaskTest(unittest.TestCase):
+    URL = 'http://127.0.0.1:5000/'
+    DATA = dict(username='kingkong', email = 'kingkong@gmail.com', password = 'kingkong@gmail.com')
     #TEST_ROUTES
     #home
-    def test_home(self):
-        tester = current_app.test_client(self)
-        response = tester.get('/home', content_type='html')
-        self.assertFalse(b'New Post' in response.data)
+    def test_1_home(self):
+        r = requests.get(FlaskTest.URL+'home')
+        self.assertFalse(b'New Post' in r.content)
 
     #about
-    def test_about(self):
-        tester = current_app.test_client(self)
-        response = tester.get('/about', content_type='html')        
-        self.assertTrue(b'This is about page.' in response.data)
+    def test_2_about(self):
+        r = requests.get(FlaskTest.URL+'about')
+        self.assertTrue(b'This is about page.' in r.content) 
 
     #register_FAILED
-    def test_register_fail(self):
-        tester = current_app.test_client(self)
-        response = tester.post('/signup', data = dict(username='kingkong', email = 'kingkong@gmail.com', password = 'kingkong@gmail.com', confirm_password = 'kingkong@gmail.com'), follow_redirects =True)
-        self.assertTrue(b'That username is already taken!' in response.data)
+    def test_3_register_fail(self):
+        r = requests.post(FlaskTest.URL+'signup', data = FlaskTest.DATA)
+        self.assertTrue(b'That username is already taken!' in r.content) 
 
     
-    #register_SUCCESS
-    def test_register(self):
-        with current_app.app_context():
-            tester = current_app.test_client(self)
-            response = tester.post('/signup', data = dict(username='admin12',email = 'admin12@gmail.com', password = 'admin12@gmail.com', confirm_password = 'admin12@gmail.com'), follow_redirects =True)
-            email = request.form.get('username')
-            print(email)
-        #self.assertNotIn('admin12',db())
+"""     #register_SUCCESS
+    def test_4_register(self):
+        r = requests.post(FlaskTest.URL+'signup', data = dict(username='king2kong', email = 'king2kong@gmail.com', password = 'king2kong@gmail.com', confirm_password = 'king2kong@gmail.com' ))
+        if 'king2kong' in db():
+            print('yes')
+        else:
+            print('no') """
+
 
 """ #signin_FAIL
     def test_login(self):
         tester = current_app.test_client(self)
         tester.post('/signin', data = dict(email = 'kingkocng@gmail.com', password = 'kingkocng@gmail.com'),follow_redirects =True)
         response = tester.get( '/signin', follow_redirects = True)
-        self.assertTrue(b'This website was built admist the Pandemic :)' in response.data)   """
+        self.assertTrue(b'This website was built admist the Pandemic :)' in response.data)   
 
-"""     #signin_SUCCESS
+    #signin_SUCCESS
     def test_login(self):
         tester = current_app.test_client(self)
-        response = tester.post('/signin', data = dict(email = 'kingkong@gmail.com', password = 'kingkong@gmail.com'), follow_redirects =True) """
+        response = tester.post('/signin', data = dict(email = 'kingkong@gmail.com', password = 'kingkong@gmail.com'), follow_redirects =True) 
        # self.assertIn(b'kingkongpost', response.data)
     
  
         #self.assertIn(b'Log In', response.data) """
-
 
 if __name__ == '__main__':
     unittest.main()
