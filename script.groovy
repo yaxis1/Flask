a1f5b1d3db59bd9f6ecb4fd625433bf0e1295c8b
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-22 04:03:26 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,6 +1,7 @@
 def install_requirements(){
     echo 'Installing requirements for flaskapp'
-    sh'sudo apt-get install python3-pip'
-    sh'sudo pip install -r requirements.txt'
+    sh'apt-get install sudo'
+    sh'echo "$USER"'
+    
 }
 return this
