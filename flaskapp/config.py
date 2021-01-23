3dc7274fd07fc8933e3f89f6d21df658f68d38b8
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-23 13:40:25 +0100

    'environment'

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -1,8 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '839c91f955762c458c526e67f90a0a68'
+    #os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    #os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
